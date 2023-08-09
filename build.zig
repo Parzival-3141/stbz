ff91d08d4a95bc82fd202e02a714a878a91edbff
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-08-09 03:42:33 -0400

    support building through zig package manager

--- build.zig
+++ build.zig
@@ -1,10 +1,40 @@
+//! To use, include `stbz` in your `build.zig` like so:
+//!```zig
+//!pub fn build(b: *Build) void {
+//!    // ...
+//!    // if using .zon dependency:
+//!    const stb_dep = b.dependency("stbz", .{ .target = target, .optimize = optimize });
+//!    // else if vendoring:
+//!    const stb_dep = b.anonymousDependency(
+//!        "your_deps/stbz/",
+//!        @import("your_deps/stbz/build.zig"),
+//!        .{ .target = target, .optimize = optimize },
+//!    );
+//!    // then add module and link library
+//!    exe.addModule("stb", stb_dep.module("stbz"));
+//!    exe.linkLibrary(stb_dep.artifact("stbz"));
+//!}
+//!```
+
+const Self = @This();
 const std = @import("std");
 const Build = std.Build;
+const LazyPath = Build.LazyPath;
 
-/// Shouldn't be called from user code
 pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    _ = b.addModule("stbz", .{ .source_file = LazyPath.relative("src/main.zig") });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "stbz",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    link_C_library(b, lib);
+    b.installArtifact(lib);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -13,63 +43,35 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    link(b, tests);
+    link_C_library(b, tests);
 
     const run_tests = b.addRunArtifact(tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
 }
 
-/// Adds module to `step` and links STB library
-pub fn link_module(b: *Build, step: *Build.Step.Compile) void {
-    step.addModule("stb", module(b));
-    link(b, step);
-}
-
-var _module: ?*Build.Module = null;
-/// Get STB module
-pub fn module(b: *Build) *Build.Module {
-    if (_module) |m| return m;
-    _module = b.createModule(.{
-        .source_file = .{ .path = stbPath("/src/main.zig") },
-    });
-    return _module.?;
-}
-
-/// Link STB library to `step`
-pub fn link(b: *Build, step: *Build.Step.Compile) void {
-    const lib = build_library(b, step.optimize, step.target);
-    step.linkLibrary(lib);
-    step.addIncludePath(Build.LazyPath.relative("include/"));
+/// Link stb C libs to `step`
+fn link_C_library(b: *Build, step: *Build.Step.Compile) void {
+    const c_lib = build_C_library(b, step.optimize, step.target);
+    step.linkLibrary(c_lib);
+    step.addIncludePath(LazyPath.relative("include/"));
     step.linkLibC();
 }
 
-fn build_library(b: *Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) *Build.Step.Compile {
+/// Build stb C libs
+fn build_C_library(b: *Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) *Build.Step.Compile {
     const lib = b.addStaticLibrary(Build.StaticLibraryOptions{
-        .name = "stb_image",
+        .name = "stb_c",
         .target = target,
         .optimize = optimize,
     });
 
-    lib.addIncludePath(Build.LazyPath.relative("include/"));
+    lib.addIncludePath(LazyPath.relative("include/"));
     lib.addCSourceFile(.{
-        .file = Build.LazyPath.relative("src/c/stb_image.c"),
+        .file = LazyPath.relative("src/c/stb_image.c"),
         .flags = &.{"-std=c99"},
     });
     lib.linkLibC();
 
     return lib;
-}
-
-fn stbPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
 }
